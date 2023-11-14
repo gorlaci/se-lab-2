f93fcb4d79b7c628a360117041ef03426de9da35
Görömbey László <laszlo.gorombey@gmail.com>
2023-11-14 21:59:11 +0100

    Added tests for TorpedoStore

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,41 @@ class TorpedoStoreTest {
 
     // Assert
     assertEquals(true, result);
+  }
+  
+  @Test
+  void fire_fail(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(1, 1);
+
+    // Act
+    boolean result = store.fire(1);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  void fire_count(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(10);
+
+    // Act
+    store.fire(1);
+    assertEquals(9, store.getTorpedoCount());
+    store.fire(2);
+    assertEquals(7, store.getTorpedoCount());
+    store.fire(3);
+    assertEquals(4, store.getTorpedoCount());
+    store.fire(4);
+    assertEquals(0, store.getTorpedoCount());
+  }
+
+  @Test
+  void fire_exception(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(1);
+
+    assertThrows(IllegalArgumentException.class, () -> store.fire(2) );
   }
 }
